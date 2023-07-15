cdc6bd6eb156969caf87fdce63675feb784389f4
Katharine McClenaghan <kemcclen@gmail.com>
2023-07-15 19:58:01 -0400

    webpack changes made to include inject manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,82 +1,10 @@
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const WebpackPwaManifest = require('webpack-pwa-manifest');
-// const path = require('path');
-// const { InjectManifest } = require('workbox-webpack-plugin');
-
-// // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// // TODO: Add CSS loaders and babel to webpack.
-
-// module.exports = () => {
-//   return {
-//     mode: 'development',
-//     entry: {
-//       main: './src/js/index.js',
-//       install: './src/js/install.js',
-//     },
-//     output: {
-//       filename: '[name].bundle.js',
-//       path: path.resolve(__dirname, 'dist'),
-//     },
-
-//     plugins: [
-//       new HtmlWebpackPlugin({
-//         template: './index.html',
-//         title: 'TypoTron',
-//       }),
-
-//       new InjectManifest({
-//         swSrc: './src-sw.js',
-//         swDest: 'src-sw.js',
-//       }),
-
-  
-//       new WebpackPwaManifest({
-//         fingerprints: false,
-//         inject: true,
-//         name: 'TypoTron',
-//         short_name: 'JATE',
-//         description: 'Simple text editor!',
-//         background_color: '#225ca3',
-//         theme_color: '#225ca3',
-//         start_url: './',
-//         publicPath: './',
-//         icons: [
-//           {
-//             src: path.resolve('src/images/logo.png'),
-//             sizes: [96, 128, 192, 256, 384, 512],
-//             destination: path.join('assets', 'icons'),
-//           },
-//         ],
-//       }),
-//     ],
-
-//     module: {
-//       rules: [
-//         {
-//           test: /\.css$/i,
-//           use: ['style-loader', 'css-loader'],
-//         },
-//         {
-//           test: /\.m?js$/,
-//           exclude: /node_modules/,
-//           // We use babel-loader in order to use ES6.
-//           use: {
-//             loader: 'babel-loader',
-//             options: {
-//               presets: ['@babel/preset-env'],
-//               plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-//             },
-//           },
-//         },
-//       ],
-//     },
-//   };
-// };
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { GenerateSW } = require('workbox-webpack-plugin');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -89,32 +17,37 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'TypoTron'
+        title: 'TypoTron',
       }),
 
-      new GenerateSW(),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+  
       new WebpackPwaManifest({
-        // TODO: Create a manifest.json:
+        fingerprints: false,
+        inject: true,
         name: 'TypoTron',
-    short_name: 'TypoTron',
-    description: 'My awesome text editor!',
-    background_color: '#ffffff',
-    start_url: './',
-    publicPath: './',
-
-    // crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
-    icons: [
-      {
-        src: path.resolve('src/images/logo.png'),
-                    sizes: [96, 128, 192, 256, 384, 512],
-                    destination: path.join('assets', 'icons'),
-      },
-    ]
+        short_name: 'JATE',
+        description: 'Simple text editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
-     
     ],
 
     module: {
@@ -126,6 +59,7 @@ module.exports = () => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
